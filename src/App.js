fd447c5003f64f2e17cc7892124e33faa23cfaf7
Ahmet Rauf OKTAY <68822176+ahmetraufoktay@users.noreply.github.com>
2024-10-18 02:34:47 +0300

    Add: NewsPage added to App.js

--- src/App.js
+++ src/App.js
@@ -4,23 +4,23 @@ import LandingPage from "./Pages/landingPage";
 import Navbar from "./Components/navbar";
 import AboutPage from "./Pages/about";
 import BoardPage from "./Pages/board";
+import NewsPage from "./Pages/news";
 import SitesPage from "./Pages/sites";
 import Footer from "./Components/footer";
 import TeamsPage from "./Pages/teams";
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="flex flex-col md:gap-0 gap-20">
-        <Navbar />
-        <LandingPage />
-        <AboutPage  />
-        <TeamsPage />
-        <SitesPage />
-        <BoardPage  />
-        <Footer />
-      </div>
-    </BrowserRouter>
+    <div id="App" className="flex flex-col md:gap-0 gap-20">
+      <Navbar />
+      <LandingPage />
+      <AboutPage />
+      <TeamsPage />
+      <NewsPage />
+      <SitesPage />
+      <BoardPage />
+      <Footer />
+    </div>
   );
 }
 
