c2a44443bfdbbea57485383a645bb82b78f3ca34
Ahmet Rauf OKTAY <68822176+ahmetraufoktay@users.noreply.github.com>
2024-10-18 01:01:37 +0300

    Fix: Change Scroll Logic using ScrollIntoView To Prevent misunderstanding of Link in react-scroll and fix react proptypes "To is undefined" error in Link I changed it into vanilla ScrollIntoView method.

--- src/Components/navbar.js
+++ src/Components/navbar.js
@@ -1,12 +1,11 @@
 import React, { useState, useEffect } from "react";
-import { Link, animateScroll as scroll } from "react-scroll";
 import skylablogo from "../Assets/Images/skylab-logo-pink.png";
 
 const Navbar = () => {
-  const [click, setClick] = useState(false);
   const [isScrolled, setIsScrolled] = useState(false);
 
   useEffect(() => {
+    // Sets scroll state for style purposes in navbar to prevent transparent navbar background when scrolled. Sets it to the background colour when scrolled.
     const handleScroll = () => {
       if (window.scrollY > 750) {
         setIsScrolled(true);
@@ -24,15 +23,23 @@ const Navbar = () => {
     };
   }, []);
 
-  const scrollToTop = () => {
-    scroll.scrollToTop();
-  };
-
+  const scrollToElement = (element) => {
+    // This code scrolls to desired element using "ID", More Info : https://developer.mozilla.org/en-US/docs/Web/API/Element/scrollIntoView
+    const scrollingElement = document.getElementById(`${element}`);
+    if (scrollingElement) {
+      scrollingElement.scrollIntoView({
+        behavior:"smooth"
+      })
+    } else {
+      return
+    }
+  }
+  
   return (
     <div
-      className={`fixed flex flex-row justify-between items-center px-3 w-full h-[100px] top-0 left-0 z-50 transition-colors duration-150 text-xl tracking-[0.10em]" duration-300 ${  isScrolled ? "bg-customDarkPurple" : "bg-transparent "} hidden md:flex`}
+      className={`fixed flex flex-row justify-between items-center px-3 w-full h-[100px] top-0 left-0 z-50 transition-colors duration-150 text-xl tracking-[0.10em]" duration-300 ${  isScrolled ?  " bg-customDarkPurple" : "bg-transparent  "} hidden md:flex`}
     >
-      <div className=" flex items-center flex-row space-x-2 lg:space-x-6 ml-10 cursor-pointer" onClick={scrollToTop}>
+      <div className=" flex items-center flex-row space-x-2 lg:space-x-6 ml-10 cursor-pointer" onClick={() => scrollToElement("App")}>
         <img alt="logo" src={skylablogo} className="w-9 h-9 lg:w-10 lg:h-10" />
         <div className="hidden lg:flex w-2 h-2 mb-[0.4rem] rounded-full bg-customLightPink"></div>
         <h1 className="font-bebasNeue text-3xl lg:text-5xl tracking-[0.3em] font-light text-customLightPink ">
@@ -41,60 +48,42 @@ const Navbar = () => {
       </div>
 
       <div className="hidden md:flex flex-row items-center gap-5 mr-10 p-2 text-shadow-md">
-        <Link
-          smooth={true}
-          offset={50}
-          duration={500}
+        <button
+          onClick={() => scrollToElement("App")}
           className="text-[#eadaff] hover:text-[#27a68e] tracking-[0.10em] px-2 py-1 cursor-pointer transition-colors duration-150 text-xl"
-          onClick={scrollToTop}
         >
           ANA SAYFA
-        </Link>
-        <Link
-          to="description"
-          smooth={true}
-          offset={50}
-          duration={500}
+        </button>
+        <button
+          onClick={() => scrollToElement("description")}
           className="text-[#eadaff] hover:text-[#27a68e] px-2 py-1 cursor-pointer transition-colors duration-150 text-xl tracking-[0.10em]"
         >
           HAKKIMIZDA
-        </Link>
-        <Link
-          to="teams"
-          smooth={true}
-          offset={50}
-          duration={500}
+        </button>
+        <button
+          onClick={() => scrollToElement("teams")}
           className="text-[#eadaff] hover:text-[#27a68e] px-2 py-1 cursor-pointer transition-colors duration-150 text-xl tracking-[0.10em]"
         >
           EKİPLER
-        </Link>
-        <Link
-          to="news"
-          smooth={true}
-          offset={50}
-          duration={500}
+        </button>
+        <button
+          onClick={() => scrollToElement("news")}
           className="text-[#eadaff] hover:text-[#27a68e] px-2 py-1 cursor-pointer transition-colors duration-150 text-xl tracking-[0.10em]"
         >
           HABERLER
-        </Link>
-        <Link
-          to="sites"
-          smooth={true}
-          offset={50}
-          duration={500}
+        </button>
+        <button
+          onClick={() => scrollToElement("sites")}
           className="text-[#eadaff] hover:text-[#27a68e] px-2 py-1 cursor-pointer transition-colors duration-150 text-xl tracking-[0.10em]"
         >
           SİTELER
-        </Link>
-        <Link
-          to="board-members"
-          smooth={true}
-          offset={50}
-          duration={500}
+        </button>
+        <button
+          onClick={() => scrollToElement("board-members")}
           className="text-[#eadaff] hover:text-[#27a68e] px-2 py-1 cursor-pointer transition-colors duration-150 text-xl tracking-[0.10em]"
         >
           YÖNETİM KURULU
-        </Link>
+        </button>
       </div>
     </div>
   );
