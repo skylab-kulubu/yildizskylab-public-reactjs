362a78b53cf3f74c10f9c5ee5daf66dda514a37d
Ahmet Rauf OKTAY <68822176+ahmetraufoktay@users.noreply.github.com>
2024-10-18 23:55:24 +0300

    Add: Create newsCarousel Create newsCarousel with using react-multi-carousel library

---
+++ src/Components/newsCarousel.js
@@ -0,0 +1,46 @@
+import NewsComponent from "./newsComponent";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+
+export default function NewsCarousel({ news, loading }) {
+  const responsive = {
+    superLargeDesktop: {
+      breakpoint: { max: 4000, min: 2000 },
+      items: 4,
+    },
+    desktop: {
+      breakpoint: { max: 2000, min: 1201 },
+      items: 3,
+    },
+    tablet: {
+      breakpoint: { max: 1200, min: 801 },
+      items: 2,
+    },
+    mobile: {
+      breakpoint: { max: 800, min: 0 },
+      items: 1,
+    },
+  };
+
+  return (
+    <Carousel responsive={responsive} infinite={true} itemClass="carousel">
+      {loading &&
+        Array.from({ length: 4 }).map((_, index) => (
+          <NewsComponent
+            key={index}
+            singleNewData={null}
+            textLoading={loading}
+          />
+        ))}
+      {news &&
+        !loading &&
+        news.map((newData) => (
+          <NewsComponent
+            key={newData.id}
+            singleNewData={newData}
+            textLoading={loading}
+          />
+        ))}
+    </Carousel>
+  );
+}
