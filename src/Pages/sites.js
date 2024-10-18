53f2ea0c63ec4d2a89b88f92ddd3a3141247aee6
Ahmet Rauf OKTAY <68822176+ahmetraufoktay@users.noreply.github.com>
2024-10-18 13:22:36 +0300

    Fix: Change grid template to flex-wrap in sites.js

--- src/Pages/sites.js
+++ src/Pages/sites.js
@@ -4,17 +4,14 @@ import sitesData from "../Data/sitesData";
 
 const SitesPage = () => {
   return (
-    <section
-      className="w-screen lg:pt-36 bg-customDarkPurple "
-      id="sites"
-    >
+    <section className="w-screen lg:pt-36 bg-customDarkPurple " id="sites">
       {/* Header */}
       <div className="w-screen flex justify-center md:mt-10 mb-24 tracking-widest mt-12">
         <h2 className="text-customAccent text-3xl lg:text-5xl">SİTELER</h2>
       </div>
 
       {/* Sites Grid */}
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 px-6 justify-items-center relative z-20 place-items-center">
+      <div className="flex flex-wrap justify-center gap-6 px-2 sm:gap-8 md:gap-12 lg:gap-32 2xl:max-w-[2000px] justify-self-center relative z-20">
         {sitesData.map((site, index) => (
           <SiteComponent key={index} site={site} />
         ))}
