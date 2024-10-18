70eae1f6b50d7aae090ecf4168e99703a9099459
Ahmet Rauf OKTAY <68822176+ahmetraufoktay@users.noreply.github.com>
2024-10-18 13:24:31 +0300

    Add: Create newsComponent with Lazy Image Loading Includes fetching animation-pulse while fetching

--- src/Components/newsComponent.js
+++ src/Components/newsComponent.js
@@ -1,16 +1,39 @@
-const NewsComponent = ({ singleNewData }) => {
+import React, { useState } from "react";
+import { LazyLoadImage } from "react-lazy-load-image-component";
+import "react-lazy-load-image-component/src/effects/blur.css";
+
+const NewsComponent = ({ singleNewData, textLoading }) => {
+  const [loading, setLoading] = useState(true);
+
+  const handleImageLoad = () => {
+    setLoading(false);
+  };
+
   return (
-    <div className="flex gap-4 box-border flex-col items-center max-w-80 text-white">
-      <div className="w-80 h-60 ">
-        <img
-          src={singleNewData.cover_image.url}
-          className="w-80 h-60 object-cover rounded-xl"
-        ></img>
+    <button
+      className={`flex gap-4 box-border flex-col items-center max-w-80 text-white border-none outline-none `}
+    >
+      <div
+        className={`w-80 h-60 bg-slate-700 rounded-xl ${
+          loading ? "animate-pulse" : ""
+        }`}
+      >
+        <LazyLoadImage
+          className={`w-80 h-60 object-cover rounded-xl $`}
+          alt={singleNewData?.title}
+          effect="blur"
+          src={singleNewData?.cover_image.url}
+          onLoad={handleImageLoad}
+        />
       </div>
-      <div className="flex items-center justify-center h-20 w-80 p-2  text-3xl text-center bg-indigo-800 rounded-xl">
-        {singleNewData.title}
+      <div
+        className={`flex items-center justify-center h-20 w-80 p-2 text-3xl text-center bg-customAccent rounded-xl ${
+          textLoading ? "animate-pulse" : ""
+        }`}
+      >
+        {singleNewData?.title}
       </div>
-    </div>
+    </button>
   );
 };
 
