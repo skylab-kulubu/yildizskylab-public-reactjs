1c209c23e628172923c0908970d15719748cfb7e
Ahmet Rauf OKTAY <68822176+ahmetraufoktay@users.noreply.github.com>
2024-10-18 23:52:50 +0300

    Fix: Remove extra padding

--- src/Pages/teams.js
+++ src/Pages/teams.js
@@ -13,10 +13,7 @@ const TeamsPage = () => {
   };
 
   return (
-    <section
-      className="h-screen snap-start relative pt-24 lg:pt-36  white "
-      id="teams"
-    >
+    <section className="h-screen snap-start relative  white " id="teams">
       <div className="w-screen flex justify-center md:mt-10 mb-16 tracking-widest ">
         <h2 className="text-customAccent text-3xl lg:text-5xl">EKİPLERİMİZ</h2>
       </div>
