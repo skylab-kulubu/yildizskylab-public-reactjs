bda5a7fbecc92d389f590cece8fb64386042986f
Ahmet Rauf OKTAY <68822176+ahmetraufoktay@users.noreply.github.com>
2024-10-18 23:50:51 +0300

    Linter: Linting Update

--- src/apis/newsFetcher.js
+++ src/apis/newsFetcher.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const newsFetcher = async (url) => {
-    const response = await axios.get(url);
-    return response.data;
-}
+  const response = await axios.get(url);
+  return response.data;
+};
