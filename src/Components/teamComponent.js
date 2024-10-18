12a6a8b99062e298871d23c15756b743738f847e
Ahmet Rauf OKTAY <68822176+ahmetraufoktay@users.noreply.github.com>
2024-10-18 13:19:14 +0300

    Fix: Change flex style to wrap in teamComponent

--- src/Components/teamComponent.js
+++ src/Components/teamComponent.js
@@ -1,39 +1,39 @@
-import React from 'react';
-import TeamsData from '../Data/teamsData';
+import React from "react";
+import TeamsData from "../Data/teamsData";
 
 const TeamComponent = ({
   teamSelect,
   teamIndex,
   setTeamIndex,
   setSelectedTeam,
-  handleScroll
+  handleScroll,
 }) => {
   return (
-    <div className='flex flex-row justify-between items-center px-12 flex-wrap snap-x snap-mandatory'>
+    <div className="flex flex-wrap justify-center items-center px-12 flex-wrap snap-x snap-mandatory">
       {TeamsData[teamSelect].map((team, index) => (
         <div
           key={index}
-          className='flex flex-col justify-center items-center space-y-4 min-w-[150px] snap-center'
+          className="flex flex-col justify-center items-center space-y-4 min-w-[150px] snap-center"
         >
           <div
-            onClick={e => {
+            onClick={(e) => {
               setTeamIndex(index);
               setSelectedTeam(team.name);
               handleScroll(e);
             }}
             className={`w-24 h-24 flex justify-center items-center ring-customAccent ${
-              teamIndex === index ? 'ring-8' : ''
+              teamIndex === index ? "ring-8" : ""
             } p-2 cursor-pointer bg-customLightPink overflow-hidden rounded-[50%]`}
           >
             <img
               src={team.logo}
               width={75}
               height={75}
-              className='object-cover rounded-full'
-              alt='logo'
+              className="object-cover rounded-full"
+              alt="logo"
             />
           </div>
-          <div className='text-customLightPink text-lg tracking-wide'>
+          <div className="text-customLightPink text-lg tracking-wide">
             {team.name}
           </div>
         </div>
