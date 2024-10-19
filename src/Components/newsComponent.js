44cff2f52201f313fef6df91eb95ac2293882abd
Ahmet Rauf OKTAY <68822176+ahmetraufoktay@users.noreply.github.com>
2024-10-19 10:44:49 +0300

    Fix: Add responsive style to newsComponent Add responsive style for devices with width lesser than 450 using sm tag (>640px)

--- src/Components/newsComponent.js
+++ src/Components/newsComponent.js
@@ -14,12 +14,12 @@ const NewsComponent = ({ singleNewData, textLoading }) => {
       className={`flex gap-4 box-border flex-col justify-self-center items-center  text-white border-none outline-none hover:opacity-75  transition delay-150 ease-in-out`}
     >
       <div
-        className={`w-80 h-60 bg-slate-700 rounded-xl ${
+        className={`w-60 h-40 sm:w-80 sm:h-60 bg-slate-700 rounded-xl ${
           loading ? "animate-pulse" : ""
         }`}
       >
         <LazyLoadImage
-          className={`w-80 h-60  object-cover rounded-xl bg-black`}
+          className={`w-60 h-40 sm:w-80 sm:h-60  object-cover rounded-xl bg-black`}
           alt={singleNewData?.title}
           effect="blur"
           src={singleNewData?.cover_image.url}
@@ -27,7 +27,7 @@ const NewsComponent = ({ singleNewData, textLoading }) => {
         />
       </div>
       <div
-        className={`h-20 w-80 text-3xl flex items-center justify-center box-border text-center bg-[#d4b8f96b] rounded-xl ${
+        className={`w-60 h-14 text-xl sm:h-20 sm:w-80 sm:text-3xl flex items-center justify-center box-border text-center bg-[#d4b8f96b] rounded-xl ${
           textLoading ? "animate-pulse" : ""
         }`}
       >
