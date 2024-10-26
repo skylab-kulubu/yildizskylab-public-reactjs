8f9bc17747b4b5844b6913c576a1057c96b2708d
Emre Uslu <75951256+rakun256@users.noreply.github.com>
2024-10-26 16:27:49 +0300

    SEO Changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
+import { BrowserRouter } from "react-router-dom";
 import "./App.css";
 import LandingPage from "./Pages/landingPage";
 import Navbar from "./Components/navbar";
 import AboutPage from "./Pages/about";
 import BoardPage from "./Pages/board";
-import NewsPage from "./Pages/news";
 import SitesPage from "./Pages/sites";
 import Footer from "./Components/footer";
 import TeamsPage from "./Pages/teams";
 
 function App() {
   return (
-    <div id="App" className="flex flex-col gap-20 sm:gap-64  overflow-x-hidden">
-      <Navbar />
-      <LandingPage />
-      <AboutPage />
-      <TeamsPage />
-      <NewsPage />
-      <SitesPage />
-      <BoardPage />
-      <Footer />
-    </div>
+    <BrowserRouter>
+      <div className="flex flex-col md:gap-0 gap-20">
+        <Navbar />
+        <LandingPage />
+        <AboutPage />
+        <TeamsPage />
+        <SitesPage />
+        <BoardPage />
+        <Footer />
+      </div>
+    </BrowserRouter>
   );
 }
 
