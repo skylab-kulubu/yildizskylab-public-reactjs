cb120a558007f3dec712b6683dc40513c3a90c76
Emre Uslu <75951256+rakun256@users.noreply.github.com>
2024-10-26 16:34:13 +0300

    Fix: App.js export

--- src/App.js
+++ src/App.js
@@ -1,27 +1,29 @@
-import { BrowserRouter } from "react-router-dom";
+// src/App.js
+
 import "./App.css";
 import LandingPage from "./Pages/landingPage";
 import Navbar from "./Components/navbar";
 import AboutPage from "./Pages/about";
 import BoardPage from "./Pages/board";
+import NewsPage from "./Pages/news";
 import SitesPage from "./Pages/sites";
 import Footer from "./Components/footer";
 import TeamsPage from "./Pages/teams";
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="flex flex-col md:gap-0 gap-20">
-        <Navbar />
-        <LandingPage />
-        <AboutPage />
-        <TeamsPage />
-        <SitesPage />
-        <BoardPage />
-        <Footer />
-      </div>
-    </BrowserRouter>
+    <div id="App" className="flex flex-col gap-20 sm:gap-64  overflow-x-hidden">
+      <Navbar />
+      <LandingPage />
+      <AboutPage />
+      <TeamsPage />
+      <NewsPage />
+      <SitesPage />
+      <BoardPage />
+      <Footer />
+    </div>
   );
 }
 
+// Eksik export default ifadesini burada ekliyoruz.
 export default App;
