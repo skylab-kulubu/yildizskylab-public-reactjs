1066266847e2fc1ad905f90cc71a9c18fea654fb
Ahmet Rauf OKTAY <68822176+ahmetraufoktay@users.noreply.github.com>
2024-10-31 00:20:18 +0300

    Fix: Change responsive font sizes in about page

--- src/Pages/about.js
+++ src/Pages/about.js
@@ -3,36 +3,33 @@ import React from "react";
 const AboutPage = () => {
   return (
     <section
-      className="flex flex-col scroll-mt-40 justify-center items-center box-border gap-20 bg-customDarkPurple"
+      className="flex flex-col scroll-mt-48 justify-center items-center box-border gap-20 bg-customDarkPurple"
       id="description"
     >
       <div className="w-screen flex justify-center tracking-widest">
         <h2 className="text-customAccent text-3xl lg:text-5xl">HAKKIMIZDA</h2>
       </div>
-      <div className="text-justify lg:text-left h-full flex justify-center sm:items-center lg:items-start px-6  md:px-12 text-xs md:text-lg lg:text-base font-inter tracking-widest ">
-        <p className="lg:w-1/2 text-customLightPink text-justify text-base md:text-lg">
-          SKY LAB Bilgisayar Bilimleri Kulübü Yıldız Teknik Üniversitesi
-          bünyesinde ar-ge,proje, organizasyon, iş geliştirme ve tanıtım
-          ekipleri ile ana yapısını oluşturan en aktif ve etkin kulüplerden
-          biridir.
-          <br />
-          <br />
-          Akran öğrenmesini baz alan workshop ve bootcamp programları ile
-          temel-orta-ileri seviyede eğitimler sunmayı, sektör-öğrenci
-          buluşmaları için büyük-küçük düzeyde seminer etkinlikleri ile
-          networking anları yaratmayı hedefler.Yapay zekadan mobile, gömülü
-          sistemlerden siber güvenliğe bilgisayar bilimlerinin 8 farklı alanını
-          besleyen ar-ge ekipleri ile öğren-uygula metodunu kullanarak proje
-          ekiplerini dinamik yapıda tutmayı hedefler.
-          <br />
-          <br />
-          Bilgisayar bilimleri ekosisteminde hem sosyalleşerek hem de öğrenip
-          geliştirerek var olmak istiyorsanız{" "}
-          <span className="text-customAccent font-bold hover:text-[#3cf6d4] transition-colors duration-150">
-            <a href="#">bize katılın!</a>
-          </span>
-        </p>
-      </div>
+      <p className="text-customLightPink px-20 font-inter tracking-widest text-base md:text-lg lg:text-2xl xl:w-4/6 text-justify">
+        SKY LAB Bilgisayar Bilimleri Kulübü Yıldız Teknik Üniversitesi
+        bünyesinde ar-ge,proje, organizasyon, iş geliştirme ve tanıtım ekipleri
+        ile ana yapısını oluşturan en aktif ve etkin kulüplerden biridir.
+        <br />
+        <br />
+        Akran öğrenmesini baz alan workshop ve bootcamp programları ile
+        temel-orta-ileri seviyede eğitimler sunmayı, sektör-öğrenci buluşmaları
+        için büyük-küçük düzeyde seminer etkinlikleri ile networking anları
+        yaratmayı hedefler.Yapay zekadan mobile, gömülü sistemlerden siber
+        güvenliğe bilgisayar bilimlerinin 8 farklı alanını besleyen ar-ge
+        ekipleri ile öğren-uygula metodunu kullanarak proje ekiplerini dinamik
+        yapıda tutmayı hedefler.
+        <br />
+        <br />
+        Bilgisayar bilimleri ekosisteminde hem sosyalleşerek hem de öğrenip
+        geliştirerek var olmak istiyorsanız{" "}
+        <span className="text-customAccent font-bold hover:text-[#3cf6d4] transition-colors duration-150">
+          <a href="#">bize katılın!</a>
+        </span>
+      </p>
     </section>
   );
 };
